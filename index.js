e041ebcb2954fcaa0788d8b6902e0fa7c289c306
Skanner33 <mathewmageto@gmail.com>
2022-11-22 12:38:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const rmMain = document.querySelector('main#main');
+rmMain.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id','victory');
+
+newHeader.textContent = 'Mathew is the champion';
